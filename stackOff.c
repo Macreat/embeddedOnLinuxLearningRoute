4f362a04897f3ff98fef48918ae0b52c8841cb7c
Macreat <mateomacreat@gmail.com>
2025-05-09 15:40:47 -0500

    adding recursivity to stack over flow on C to see the performance on registers and re calls from my main function

--- stackOff.c
+++ stackOff.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
+// now proceed with the recurisivy ¿? 
+
+
+unsigned Long int suma (unsigned Long int i)
+{
+    if (i==1) return 1;
+    else return (i + suma(i-1))
+}
 
 int main ( 
     {
             unsigned long int tope = 1000;
-            unsigned long int suma = 0; 
-            for (unsigned lon int i=0; i<tope; i++)
-                    suma+= i; 
-            print ("suma: %u\n\r", suma);
+            unsigned long int result  = suma(tope-1); 
+           // for (unsigned lon int i=0; i<tope; i++)
+        //       suma+= i; 
+            print ("suma: %u\n\r",result );
             return 0; 
     }
 )
