8b0f0801050d9aaa6358c7f74e0957d1bd16bb7e
Macreat <mateomacreat@gmail.com>
2025-05-09 15:31:41 -0500

    adding content and changes from hello file to see compute process

--- hello.c
+++ hello.c
@@ -2,7 +2,10 @@
 
 int main(int argc, char *argv[])
 {
- 
+    if (argc == 1)
+        printf("Se necesitan argumentos!\n\r"); // \n is a new line
+    else if (argc == 2)
+        printf("Hello, %s!\n", argv[1]); // \n is a new line
     printf("Hello, World!\n\r"); // undesrtading the \n\r as a typewriter   
      return 0;
 }
